357fd91ee1cac0e4a93e03ecdd0fb257c3a09038
ishubham4 <89272654+ishubham4@users.noreply.github.com>
2023-05-30 03:14:33 +0530

    Add files via upload

---
+++ js/typeWriter.js
@@ -0,0 +1,36 @@
+
+let i = 0;
+let text1 = "Hey!  My Love.";
+let text2 = "In the depths of your eyes, I discovered a universe where love knows no boundaries, where souls intertwine and hearts beat in perfect harmony."
+let speed = 100;
+
+function typeWriter(text, para){
+	if(ok == 2){
+		clearInterval(typeInterval);
+	}
+	if(i < text.length){
+		document.getElementById(para).innerHTML += text.charAt(i);
+		i++;
+		speed = Math.random() * 50 + 100;
+	}
+	else{
+		if(ok == 0){
+			i = 0;
+		}
+		ok += 1;
+	}
+}
+
+var typeInterval;
+
+//window.onload = function() {
+//	window.onload = function(){};
+   	typeInterval = setInterval(function(){
+		if(ok == 0){
+			typeWriter(text1, "txt1");
+		}
+		else if(ok == 1){
+			typeWriter(text2, "txt2");
+		}
+	}, 100);
+//};
